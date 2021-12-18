1c233fac7624e21ab96b954cc0869f7f3a42b8e0
Dominic Luidold <dominic.luidold@students.fhv.at>
2021-12-18 16:48:50 +0100

    Use default "noDetectionDistance" and improve wall following

--- Behavior/FollowWallBeavhior.py
+++ Behavior/FollowWallBeavhior.py
@@ -4,16 +4,20 @@ from Behavior.BaseBehavior import BaseBehavior
 class FollowWallBehavior(BaseBehavior):
 
     def is_applicable(self, distances: list, no_detection_distance: int) -> bool:
-        return distances[2] < 0.2
+        return distances[2] < 0.05
 
     def calculate_motor_value(self, distances: list, no_detection_distance: int) -> (float, float):
         # Initial values
         vel_left = 1
         vel_right = 1
 
-        print("Sensor 2: ", distances[2])
-
-        vel_left = vel_left + (1 - (distances[2] / no_detection_distance)) * 0.9
+        # Calculate velocity proportionally
+        print("Following Wall")
+        vel_left = vel_left + (1 - (distances[2] / no_detection_distance)) * 1.0
         vel_right = vel_right + (1 - (distances[2] / no_detection_distance)) * -0.6
+
+        # Counter-steer additionally if wall in front is detected
+        vel_left = vel_left + (1 - (distances[4] / no_detection_distance)) * 0.6
+        vel_right = vel_right + (1 - (distances[4] / no_detection_distance)) * -0.4
 
         return vel_left, vel_right
--- Behavior/ReturnToWallBehavior.py
+++ Behavior/ReturnToWallBehavior.py
@@ -4,14 +4,16 @@ from Behavior.BaseBehavior import BaseBehavior
 class ReturnToWallBehavior(BaseBehavior):
 
     def is_applicable(self, distances: list, no_detection_distance: int) -> bool:
-        return distances[2] >= 0.2
+        return distances[2] >= 0.05
 
     def calculate_motor_value(self, distances: list, no_detection_distance: int) -> (float, float):
+        # Initial values
         vel_left = 1
         vel_right = 1
 
-        print("Returning to Wall - Sensor 2", distances[2])
-
-        vel_right = vel_right + 0.3
+        # Calculate velocity proportionally
+        print("Returning to Wall")
+        vel_left = vel_left + (1 - (distances[2] / no_detection_distance)) * 0.7
+        vel_right = vel_right + (1 - (distances[2] / no_detection_distance)) * 1.3
 
         return vel_left, vel_right
